fb7259e7755e066a7a6703bd02364b6e90776a31
teomankirma <83593653+teomankirma@users.noreply.github.com>
2022-04-16 19:48:25 +0300

    Bubble Sort

---
+++ BubbleSort/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace BubbleSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = {"teo", "jack", "michael", "adam", "daniel"};
+
+            BubbleSort.Sort(names);
+
+            Console.Write("Bubble Sort");
+            Console.WriteLine(" ");
+            Console.WriteLine(string.Join(" | ", names));
+        }
+    }
+}
